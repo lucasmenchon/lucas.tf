57f31e2c8127583a950992ae079fe0d42d5b1101
lucasmenchon <developer@luccas.dev>
2024-01-23 21:08:01 -0300

    update to .net 8.0

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using BlazorWebProfile.Models;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using Blazorise;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,7 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddSingleton<ThemeService>();
 builder.Services.AddLocalization();
-builder.Services.AddBlazorise();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
