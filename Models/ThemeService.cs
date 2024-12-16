e15a73bf27b9d467134642ffc7b0915430eb35e7
lucasmenchon <it.lucas@outlook.com>
2024-12-16 01:58:48 -0300

    Update packages, refactor components, and improve UI
    Updated BlazorWebProfile.csproj to use newer versions of MudBlazor and Microsoft.AspNetCore.Components.WebAssembly packages. Refactored Index.razor by modularizing content into new components (HeaderSection, SocialLinksSection, EducationSection, ExperienceSection, TestimonialsSection, SkillsSection, ProjectsSection, ContactSection, SectionTitle). Improved ThemeService with new properties and methods. Enhanced NavMenu.razor to use updated ThemeService and IScrollManager. Moved and added CSS classes for better styling. Added meta tags and preload link in index.html for SEO and performance. Added global using directive for MudBlazor.

--- Models/ThemeService.cs
+++ Models/ThemeService.cs
@@ -2,20 +2,34 @@
 {
     public class ThemeService
     {
-        public bool DarkMode { get; set; }
+        public bool DarkMode { get; set; } = false;
+
+        public MudTheme CustomTheme => new MudTheme()
+        {
+            PaletteLight = new PaletteLight()
+            {
+                Primary = Colors.Blue.Darken2,
+                Secondary = Colors.DeepPurple.Darken1,
+                Background = Colors.Gray.Lighten4,
+                AppbarBackground = Colors.Blue.Darken3,
+                TextPrimary = Colors.Gray.Darken4
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = Colors.Blue.Lighten3,
+                Secondary = Colors.Purple.Lighten2,
+                Background = Colors.Gray.Darken4,
+                AppbarBackground = Colors.Gray.Darken3,
+                TextPrimary = Colors.Gray.Lighten1
+            }
+        };
 
         public event Action<bool>? OnThemeChanged;
 
-        public ThemeService()
-        {
-            DarkMode = true;
-        }
-
-        public bool ToggleTheme()
+        public void ToggleTheme()
         {
             DarkMode = !DarkMode;
             OnThemeChanged?.Invoke(DarkMode);
-            return DarkMode;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddSingleton<ThemeService>();
+builder.Services.AddScoped<IScrollManager, ScrollManager>();
 builder.Services.AddLocalization();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
